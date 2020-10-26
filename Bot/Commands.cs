c10d36366b308c5859cbcf46547e2d53ecbaaa86
HaydenMeloche <meloche.hayden@gmail.com>
2020-10-25 23:04:46 -0400

    feat(bot): initial version of bot
    Bot currently supports starting a game with the start command which will
    hook up the SDK into AmongUs. Once that has happend, events are
    subscribed to for actions such as joining a server, game state change.
    
    Right now the discrod bot will post a message when a lobby is joined
    which includes the access code and region. The other feature is an auto
    mute system in which the bot will automatically mute players during play
    time and unmute alive players during voting/disucussion points in the
    game. Bot is still very rough around the edges and a lot of improvements
    can be made.

---
+++ Bot/Commands.cs
@@ -0,0 +1,117 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        private ISocketMessageChannel channel;
+        private SocketUser lobbyCreator;
+        private SocketVoiceChannel voiceChannel;
+        private static Dictionary<String, ulong> discordAmongUsName = new Dictionary<string, ulong>();
+
+        [Command("connect")]
+        [Alias("start", "play")]
+        public Task PingAsync()
+        {
+
+            var voiceChannels = Context.Guild.VoiceChannels.Where(channel => channel.Users.Any(a => a.Id.Equals(Context.Message.Author.Id)));
+
+            if (voiceChannels.Count() != 1)
+                return ReplyAsync($"{Context.Message.Author.Mention} You must be connected to a voice channel before starting a game");
+            
+            ReplyAsync("connecting to local among us instance...");
+
+            Task.Factory.StartNew(() => AmongUsReader.getInstance().Run());
+            AmongUsReader.getInstance().GameStateChanged += GameStateChangedHandler;
+            AmongUsReader.getInstance().PlayerChanged += PlayerChangedHandler;
+            AmongUsReader.getInstance().JoinedLobby += JoinedLobbyHandler;
+            AmongUsReader.getInstance().ChatMessageAdded += ChatMessageHandler;
+
+            channel = Context.Channel;
+            lobbyCreator = Context.User;
+            voiceChannel = voiceChannels.First();
+
+
+            return ReplyAsync("Sucessfully connected to local among us instance. Use `set-name {name}` to set your in game name if it is different then your discord name");
+        }
+
+        [Command("set-name")]
+        public Task PingAsync(String inGameName)
+        {
+            discordAmongUsName.Add(inGameName.ToLower(), Context.Message.Author.Id);
+            return ReplyAsync($"{Context.Message.Author.Mention} set `{inGameName}` as your in game name.");
+        }
+
+        private void ChatMessageHandler(object sender, ChatMessageEventArgs e)
+        {
+            
+        }
+
+        private void JoinedLobbyHandler(object sender, LobbyEventArgs e)
+        {
+            var embed = new EmbedBuilder()
+            .WithTitle("New Lobby Created")
+            .WithImageUrl("https://cdn.discordapp.com/avatars/770073344468713572/3b7966fccd41b4572839a49db914508b.png")
+            .WithFooter(footer => footer.Text = "AmongUs Moderator")
+            .WithColor(Color.Blue)
+            .WithCurrentTimestamp()
+            .AddField("Lobby Code", e.LobbyCode)
+            .AddField("Region", e.Region);
+
+            channel.SendMessageAsync(embed: embed.Build());
+        }
+
+        private void PlayerChangedHandler(object sender, PlayerChangedEventArgs e)
+        {
+
+
+        }
+
+        private void GameStateChangedHandler(object sender, GameStateChangedEventArgs e)
+        {
+            if (e.NewState == GameState.DISCUSSION)
+            {
+                var alivePlayers = AmongUsReader.getInstance().GetAlivePlayers();
+                foreach (var player in alivePlayers)
+                {
+                    ulong userId;
+                    if (discordAmongUsName.TryGetValue(player.Key, out userId))
+                    {
+                        voiceChannel.Users.Where(x => x.Id == userId).First().ModifyAsync(x =>
+                        {
+                            x.Mute = false;
+                        });
+                    } else
+                    {
+                        IEnumerable<SocketGuildUser> user = voiceChannel.Users.Where(x => x.Nickname.ToLower() == player.Key); 
+                        if (user.Count() == 1)
+                        {
+                            user.First().ModifyAsync(x =>
+                            {
+                                x.Mute = false;
+                            });
+                        }
+                    }
+                }
+
+            } else if (e.NewState == GameState.TASKS)
+            {
+                foreach (var user in voiceChannel.Users)
+                {
+                    user.ModifyAsync(x =>
+                    {
+                        x.Mute = true;
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+            public async Task MainAsync()
+            {
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                // Tokens should be considered secret data and never hard-coded.
+                // We can read from the environment variable to avoid hardcoding.
+                await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+                await client.StartAsync();
+
+                // Here we initialize the logic required to register our commands.
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+
+
+
+                await Task.Delay(Timeout.Infinite);
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .BuildServiceProvider();
+        }
+    }
+}
